ef39c3893cf07396eabbb762278875d1919fcc05
Kevin Boss <kevin.boss@outlook.com>
2016-11-02 01:16:17 +0100

    Set up project and added source, created basic package structure

---
+++ src/ch/hslu/ai/connect4/players/AIPlayerK.java
@@ -0,0 +1,22 @@
+package ch.hslu.ai.connect4.players;
+
+import ch.hslu.ai.connect4.Player;
+
+/**
+ * Created by kevin_emgquz4 on 02.11.2016.
+ */
+public class AIPlayerK extends Player {
+    /**
+     * Constructor:
+     *
+     * @param name The name of this player.
+     */
+    public AIPlayerK(String name) {
+        super(name);
+    }
+
+    @Override
+    public int play(char[][] board) {
+        return 0;
+    }
+}
